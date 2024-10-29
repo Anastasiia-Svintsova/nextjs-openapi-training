9b9688d7e9942b28875bbdac29ab9cbdfe93dfd6
Anastasiia Svintsova <svintsova.nastya@gmail.com>
2024-10-29 22:41:15 +0100

    feat(counter): add ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import Link from 'next/link';
+
 const HomePage = () => {
   return (
     <div>
-      <h1 className="text-7xl">Home Page</h1>
+      <h1 className="mb-8 text-5xl font-bold">Next.js tutorial</h1>
+
+      <Link href="/client" className="btn btn-accent">
+        Get started
+      </Link>
     </div>
   );
 };
