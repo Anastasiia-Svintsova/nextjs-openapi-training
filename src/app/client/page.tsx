263db7a36bd11366018aa0a0c4273d8409db10a4
Anastasiia Lantratova <99403198+Anastasiia-Svintsova@users.noreply.github.com>
2024-10-29 22:42:28 +0100

    feat(counter): add ui

--- src/app/client/page.tsx
+++ src/app/client/page.tsx
@@ -1,7 +1,26 @@
+'use client';
+
+import { useState } from 'react';
+
 const ClientPage = () => {
+  const [count, setCount] = useState(0);
+
+  const onIncreaseClick = () => setCount((prev) => prev + 1);
+  const onDecreaseClick = () => setCount((prev) => prev - 1);
+
   return (
-    <div>
-      <h1 className="text-7xl">Client Page</h1>
+    <div className="flex flex-col items-center">
+      <h1 className="mb-4 text-7xl font-bold">{count}</h1>
+
+      <div className="flex gap-4">
+        <button className="btn btn-primary" onClick={onIncreaseClick}>
+          increase
+        </button>
+
+        <button className="btn btn-secondary" onClick={onDecreaseClick}>
+          decrease
+        </button>
+      </div>
     </div>
   );
 };
