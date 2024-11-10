1d5f41323867a59a2fc499e730927534aa2d765e
Anastasiia Svintsova <svintsova.nastya@gmail.com>
2024-11-10 16:25:53 +0100

    feat(drinks): implement single drink page

--- src/app/drinks/[id]/page.tsx
+++ src/app/drinks/[id]/page.tsx
@@ -1,5 +1,41 @@
-const SingleDrinkPage = ({ params }: { params: { id: string } }) => {
-  return <div>{`SingleDrinkPage ${params.id}`}</div>;
+import { singleDrinkUrl } from '@/app/constants';
+import { Drink } from '@/shared';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const getSingleDrink = async (id: string): Promise<{ drinks: Drink[] }> => {
+  const response = await fetch(`${singleDrinkUrl}${id}`);
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch a drink...');
+  }
+
+  return response.json();
+};
+
+const SingleDrinkPage = async ({ params }: { params: { id: string } }) => {
+  const data = await getSingleDrink(params.id);
+
+  const { strDrink: title, strDrinkThumb: imgSrc } = data.drinks[0] || {};
+
+  return (
+    <div>
+      <Link href="/drinks" className="btn btn-primary mb-12 mt-8">
+        back to drinks
+      </Link>
+
+      <Image
+        src={imgSrc}
+        alt={title}
+        className="mb-4 rounded-lg shadow-lg"
+        priority
+        width={192}
+        height={192}
+      />
+
+      <h1 className="mb-8 text-4xl">{title}</h1>
+    </div>
+  );
 };
 
 export default SingleDrinkPage;
--- src/app/drinks/page.tsx
+++ src/app/drinks/page.tsx
@@ -3,10 +3,7 @@ import { drinksUrl } from '../constants';
 import { Drink } from '@/shared';
 
 const fetchDrinks = async (): Promise<{ drinks: Drink[] }> => {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await fetch(drinksUrl);
-
-  // throw error
 
   if (!response.ok) {
     throw new Error('Failed to fetch drinks');
