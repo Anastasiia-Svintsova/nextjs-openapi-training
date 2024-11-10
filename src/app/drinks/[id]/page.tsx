5583fe9ee6d7fb7f40ec031c84a0f5465a784954
Anastasiia Lantratova <99403198+Anastasiia-Svintsova@users.noreply.github.com>
2024-11-10 15:56:21 +0100

    feat(drinks): implement list

---
+++ src/app/drinks/[id]/page.tsx
@@ -0,0 +1,5 @@
+const SingleDrinkPage = ({ params }: { params: { id: string } }) => {
+  return <div>{`SingleDrinkPage ${params.id}`}</div>;
+};
+
+export default SingleDrinkPage;
--- src/app/drinks/page.tsx
+++ src/app/drinks/page.tsx
@@ -1,7 +1,28 @@
-const DrinksPage = () => {
+import DrinksList from '@/components/DrinksList';
+import { drinksUrl } from '../constants';
+import { Drink } from '@/shared';
+
+const fetchDrinks = async (): Promise<{ drinks: Drink[] }> => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const response = await fetch(drinksUrl);
+
+  // throw error
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch drinks');
+  }
+
+  const data = await response.json();
+
+  return data;
+};
+
+const DrinksPage = async () => {
+  const data = await fetchDrinks();
+
   return (
     <div>
-      <h1 className="text-7xl">Drinks Page</h1>
+      <DrinksList drinks={data.drinks} />
     </div>
   );
 };
